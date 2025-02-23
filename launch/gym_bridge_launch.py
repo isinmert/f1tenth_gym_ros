0ca6aa824bd917126235f3d7b0606f7dec44c21d
isinmert <isinmertbalci@gmail.com>
2025-02-23 06:24:58 +0000

    fix map filepath

--- launch/gym_bridge_launch.py
+++ launch/gym_bridge_launch.py
@@ -44,16 +44,12 @@ def generate_launch_description():
         name='bridge',
         parameters=[config]
     )
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz',
-        arguments=['-d', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'gym_bridge.rviz')]
-    )
     map_server_node = Node(
         package='nav2_map_server',
         executable='map_server',
-        parameters=[{'yaml_filename': config_dict['bridge']['ros__parameters']['map_path'] + '.yaml'},
+        parameters=[{'yaml_filename': 
+                     config_dict['bridge']['ros__parameters']['map_path'] 
+                     + '.yaml'},
                     {'topic': 'map'},
                     {'frame_id': 'map'},
                     {'output': 'screen'},
@@ -72,19 +68,28 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='ego_robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'ego_racecar.xacro')])}],
+        parameters=[{'robot_description': 
+                     Command(['xacro ', 
+                              os.path.join(
+                                  get_package_share_directory(
+                                      'f1tenth_gym_ros'), 
+                                  'launch', 'ego_racecar.xacro')])}],
         remappings=[('/robot_description', 'ego_robot_description')]
     )
     opp_robot_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='opp_robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'opp_racecar.xacro')])}],
+        parameters=[{'robot_description': 
+                     Command(['xacro ', 
+                              os.path.join(
+                                  get_package_share_directory(
+                                      'f1tenth_gym_ros'), 
+                                      'launch', 'opp_racecar.xacro')])}],
         remappings=[('/robot_description', 'opp_robot_description')]
     )
 
     # finalize
-    ld.add_action(rviz_node)
     ld.add_action(bridge_node)
     ld.add_action(nav_lifecycle_node)
     ld.add_action(map_server_node)
